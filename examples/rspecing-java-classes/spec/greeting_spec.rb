fc1d9bd43e526262081b17b4ab5b7479676c4aa5
Mateus Linhares <mateuslinhares@gmail.com>
2012-05-30 10:42:06 -0300

    melhorei o exemplo

--- examples/rspecing-java-classes/spec/greeting_spec.rb
+++ examples/rspecing-java-classes/spec/greeting_spec.rb
@@ -3,9 +3,16 @@ require "java"
 java_import "com.examples.Greeting"
 
 describe Greeting do
-  let(:greeting){ Greeting.new }
+  let(:greeting){ Greeting.new "Mateus Linhares", "mateus@idopter.com" }
+  let(:date){ Time.now.strftime "%d/%m/%Y" }
 
-  it "should say hello" do
-    greeting.say_hello.should == "Hello from Java class!"
+  it "should say hello to me" do
+    greeting.say_hello.should == "Hello, Mateus Linhares. Your email: mateus@idopter.com. Date: #{date}"
+  end
+
+  it "should say hello to Piguera" do
+    greeting.set_name "Piguera"
+    greeting.set_email "pigor@idopter.com"
+    greeting.say_hello.should == "Hello, Piguera. Your email: pigor@idopter.com. Date: #{date}"
   end
 end
--- examples/rspecing-java-classes/src/com/examples/Greeting.java
+++ examples/rspecing-java-classes/src/com/examples/Greeting.java
@@ -1,7 +1,39 @@
 package com.examples;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Greeting{
+	private String name;
+	private String email;
+
+	public Greeting(String name, String email){
+		this.name = name;
+		this.email = email;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+	public String getEmail(){
+		return this.email;
+	}
+
 	public String sayHello(){
-		return "Hello from Java class!";
+		new SimpleDateFormat("dd/MM/yyyy");
+		String hello = "Hello, " + this.name;
+		hello += ". Your email: " + this.email;
+		hello += ". Date: " + new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+
+		return hello;
 	}
 }
