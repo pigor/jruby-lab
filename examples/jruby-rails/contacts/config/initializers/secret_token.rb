03a10b563169f75d087aed382beba71e08f6f431
Paulo Igor <piagodinho@gmail.com>
2012-06-07 15:36:13 -0300

    Organizando os exemplos

---
+++ examples/jruby-rails/contacts/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contacts::Application.config.secret_token = 'a592c4a9a7e2f5937bdcb8c675ffc6eb16767e2640f23ce178508c600086bf9ef4b01a00e7e23e171cf49a61a695996a925535655d4b606c5f775de8c9290967'
