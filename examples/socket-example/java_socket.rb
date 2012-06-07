cee6394140f07877f39bc9ab43f31575a8d96dcd
Paulo Igor <piagodinho@gmail.com>
2012-06-07 15:37:08 -0300

    Organizando os exemplos

---
+++ examples/socket-example/java_socket.rb
@@ -0,0 +1,8 @@
+module JavaSocket
+  import "java.lang"
+  import "java.io"
+  
+  # outro forma de incluir os pacotes
+  include_package "java.net"
+  include_package "java.util"
+end
--- examples/socket_example/java_socket.rb
+++
@@ -1,6 +0,0 @@
-module JavaSocket
-  import "java.lang"
-  import "java.io"
-  import "java.net"
-  import "java.util"
-end
